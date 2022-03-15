a840ce580b0d961d63c56d84fbfc1f4e8099e16b
Michael Tamirie <michaeltamirie@gmail.com>
2022-03-15 14:37:17 +0300

    New

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,13 +9,13 @@ int _putchar(char c)
 	return (write(1, &c, 1));
 }
 int main(void){
-char WRITE[]="_putchar";
-int i=0;
-while (WRITE[i] != '\0')
-{
-_putchar(WRITE[i]);
-i++;
-}
-_putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 return (0);
 }
