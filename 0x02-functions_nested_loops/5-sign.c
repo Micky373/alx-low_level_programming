51096a4cd48b5ca6b7d1ce82c88e87941d7da94b
Michael Tamirie <michaeltamirie@gmail.com>
2022-03-15 16:21:18 +0300

    New

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * print_sign - Return 1 if number is positive, -1 if negative and 0 if it is zero.
- * @c: char type letter
+ * print_sign - Return 1,0 or -1 depending on the sign
+ * @n: char type letter
  * Return: 1 if lowercase, 0 if not lowercase
  */
 int print_sign(int n)
@@ -11,7 +11,7 @@ if (n > 0)
 _putchar('+');
 return (1);
 }
-else if (n<0)
+else if (n < 0)
 {
 _putchar('-');
 return (-1);
