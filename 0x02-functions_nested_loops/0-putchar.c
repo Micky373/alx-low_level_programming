7bfb5f1066e4d5762ee5952de6d9bf53ea0735a2
Michael Tamirie <michaeltamirie@gmail.com>
2022-03-15 14:06:48 +0300

    Edit some thing

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
 int main(void){
-char Micky[]="_putchar";
+char WRITE[]="_putchar";
 int i=0;
-while (Micky[i] != '\0')
+while (WRITE[i] != '\0')
 {
-_putchar(Micky[i]);
+_putchar(WRITE[i]);
 i++;
 }
 _putchar('\n');
