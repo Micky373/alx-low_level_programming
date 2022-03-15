1162ae9045edea41ecdf27252d40724f61edaf83
Michael Tamirie <michaeltamirie@gmail.com>
2022-03-15 13:56:14 +0300

    Edit some thing

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,17 +1,11 @@
 #include<stdio.h>
+#include<unistd.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-void _putchar(void) {
-    putchar ('_');
-    putchar ('p');
-    putchar ('u');
-    putchar ('t');
-    putchar ('c');
-    putchar ('h');
-    putchar ('a');
-    putchar ('r');
-    putchar ('\n');
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
