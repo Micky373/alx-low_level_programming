5bc1f084d83138b62cab6338be64349c59d5574f
Michael Tamirie <michaeltamirie@gmail.com>
2022-03-15 13:52:30 +0300

    Edit some thing

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,13 +7,13 @@
  * Return: Always 0.
  */
 int main(void){
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
+char Micky[]="_putchar";
+int i=0;
+while (Micky[i] != '\0')
+{
+_putchar(Micky[i]);
+i++;
+}
+_putchar('\n');
 return (0);
 }
