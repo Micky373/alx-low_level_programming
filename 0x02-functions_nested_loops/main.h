fe959188dc61b46707d4ac7dc9ea03b0303ffdad
Michael Tamirie <michaeltamirie@gmail.com>
2022-03-15 14:46:16 +0300

    New

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,12 +6,4 @@
  * Return: Always 0 (Success)
  */
 int _putchar (char c);
-void print_alphabet(void)
-{
-char ch = 'a';
-for (ch = 'a'; ch <= 'z'; ch++)
-{
-putchar(ch);
-}
-putchar('\n');
-}
+void print_alphabet(void);
