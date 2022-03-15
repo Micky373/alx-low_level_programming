f4e78a918c4388c56db6e5cf5a8a0494077545ab
Michael Tamirie <michaeltamirie@gmail.com>
2022-03-15 15:38:15 +0300

    New

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * main - Entry point
- * printalphabet - print lowercase alphabets
+ * print_alphabet - print lowercase alphabets
  * Return: Always 0 (Success)
  */
 void print_alphabet(void)
