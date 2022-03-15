a3a1ac6e79bddaadd1956e5546ac3e54bc9f6e7a
Michael Tamirie <michaeltamirie@gmail.com>
2022-03-15 14:59:13 +0300

    New

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,12 @@
 #include "main.h"
+void print_alphabet(void) {
+char ch = 'a';
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+}
 int main (void)
 {
 print_alphabet();
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,11 +6,3 @@
  * Return: Always 0 (Success)
  */
 int _putchar (char c);
-void print_alphabet(void) {
-char ch = 'a';
-for (ch = 'a'; ch <= 'z'; ch++)
-{
-putchar(ch);
-}
-putchar('\n');
-}
