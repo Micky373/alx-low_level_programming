a1310bcbf580022ea121b64094dbaf2afdd27fca
Michael Tamirie <michaeltamirie@gmail.com>
2022-03-15 16:25:43 +0300

    New

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include<stdlib.h>
+#include "main.h"
+/**
+ * _abs - Return absolute value of a number
+ * @n: char type letter
+ * Return: absolute value of the number
+ */
+int _abs(int n)
+{
+return abs(n);
+}
