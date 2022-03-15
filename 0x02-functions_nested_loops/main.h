50bfa8ae8e4e30432c10b4aa36af9a797223d346
Michael Tamirie <michaeltamirie@gmail.com>
2022-03-15 14:45:26 +0300

    New

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,17 @@
 #include<unistd.h>
+#include<stdio.h>
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int _putchar (char c);
+void print_alphabet(void)
+{
+char ch = 'a';
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+}
