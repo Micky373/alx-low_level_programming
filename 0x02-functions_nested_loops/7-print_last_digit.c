659dfe8e8074f7657a8b7d94695fb6b82162e933
Michael Tamirie <michaeltamirie@gmail.com>
2022-03-15 16:33:44 +0300

    New

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_last_digit - Return 1,0 or -1 depending on the sign
+ * @n: char type letter
+ * Return: 1 if lowercase, 0 if not lowercase
+ */
+int print_last_digit(int)
+{
+return (int%10)
+}
