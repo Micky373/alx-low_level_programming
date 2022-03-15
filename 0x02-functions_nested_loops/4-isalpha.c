bfabacc80bb05ee939483eee16544f5a1d7c12c9
Michael Tamirie <michaeltamirie@gmail.com>
2022-03-15 16:07:48 +0300

    New

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - Return 1 if letter is lowercase, 0 if not.
+ * @c: char type letter
+ * Return: 1 if lowercase, 0 if not lowercase
+ */
+int _isalpha(int c)
+{
+if (( c > 96 && c < 123 ) || (c > 64 && c <92 ))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
