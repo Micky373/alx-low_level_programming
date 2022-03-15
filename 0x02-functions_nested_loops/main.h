eaddc6de8e2a7f943cf3d92d417f9892ab2e5f28
Michael Tamirie <michaeltamirie@gmail.com>
2022-03-15 13:14:40 +0300

    Add readme file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void _putchar(void) {
+    putchar ('_');
+    putchar ('p');
+    putchar ('u');
+    putchar ('t');
+    putchar ('c');
+    putchar ('h');
+    putchar ('a');
+    putchar ('r');
+    putchar ('\n');
+}
